a9c759914a5cb9db0689ea89558d3ed6d0c2a4d8
Stephane Roy <stephroy411@hotmail.com>
2021-10-24 12:22:34 -0300

    Add Javascript main.js with modules CurrentLocation.js(CurrentLocation constructor) and domFunctions.js(animate spinner)

---
+++ dist/js/main.js
@@ -0,0 +1,60 @@
+import { addSpinner } from "./domFunctions.js";
+import CurrentLocation from "./CurrentLocation.js";
+const currentLoc = new CurrentLocation();
+
+const initApp = () => {
+  // add listeners
+  const geoButton = document.getElementById("getLocation");
+  geoButton.addEventListener("click", getGeoWeather);
+  // setup
+
+  // load weather
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+
+const getGeoWeather = (event) => {
+  if (event && event.type === "click") {
+    const mapIcon = document.querySelector(".fa-map-marker-alt");
+    addSpinner(mapIcon);
+  }
+  if (!navigator.geolocation) return geoError();
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+};
+
+
+
+
+
+
+
+
+        // <!-- Hard coded data...to do: generate content dynamically with js and API -->
+        // <div class="icon" id="icon"><i class="far fa-sun" aria-hidden="true" title="clear sky"></i></div>
+        // <div class="temp">11°<div class="unit">C</div></div>
+        // <div class="desc">Clear Sky</div>
+        // <div class="feels">Feels Like 9°</div>
+        // <div class="maxtemp">High 13°</div>
+        // <div class="mintemp">Low 7°</div>
+        // <div class="humidity">Humidity 44%</div>
+        // <div class="wind">wind 7 mph</div>
+
+
+
+      //    <!-- Hard coded data...to do: generate content dynamically with js and API -->
+      //    <div class="forecastDay"><p class="dayAbbreviation">SUN</p><img src="https://openweathermap.org/img/wn/03d.png" alt="scattered clouds"><p
+      //    class="dayHigh">61°</p><p class="dayLow">44°</p></div>
+      //  <div class="forecastDay"><p class="dayAbbreviation">MON</p><img src="https://openweathermap.org/img/wn/03d.png" alt="scattered clouds"><p
+      //    class="dayHigh">62°</p><p class="dayLow">46°</p></div>
+      //  <div class="forecastDay"><p class="dayAbbreviation">TUE</p><img src="https://openweathermap.org/img/wn/04d.png" alt="overcast clouds"><p
+      //    class="dayHigh">61°</p><p class="dayLow">52°</p></div>
+      //  <div class="forecastDay"><p class="dayAbbreviation">WED</p><img src="https://openweathermap.org/img/wn/10d.png" alt="moderate rain"><p
+      //    class="dayHigh">70°</p><p class="dayLow">60°</p></div>
+      //  <div class="forecastDay"><p class="dayAbbreviation">THU</p><img src="https://openweathermap.org/img/wn/10d.png" alt="moderate rain"><p
+      //    class="dayHigh">61°</p><p class="dayLow">44°</p></div>
+      //  <div class="forecastDay"><p class="dayAbbreviation">FRI</p><img src="https://openweathermap.org/img/wn/03d.png" alt="scattered clouds"><p
+      //    class="dayHigh">61°</p><p class="dayLow">44°</p></div>
+      //  <div class="forecastDay"><p class="dayAbbreviation">SAT</p><img src="https://openweathermap.org/img/wn/03d.png" alt="scattered clouds"><p
+      //    class="dayHigh">61°</p><p class="dayLow">44°</p></div>
+
